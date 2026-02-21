ada97cb903c857e4a1989b55b8983b6a85f9e72d
Tillah-tech <aliciamuyathi@gmail.com>
2026-02-21 20:47:21 +0300

    Add files via upload

---
+++ Assignment week four do while.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    char password[20];
+
+    do {
+        printf("Enter password: ");
+        scanf("%s", password);
+
+        if(strcmp(password, "1234") != 0) {
+            printf("Incorrect password. Try again.\n");
+        }
+
+    } while(strcmp(password, "1234") != 0);
+
+    printf("Access Granted\n");
+
+    return 0;
+}
