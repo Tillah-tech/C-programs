8acce9cbaf148b52389e785d78d3edb9dd2c72c2
Tillah-tech <aliciamuyathi@gmail.com>
2026-02-21 20:41:49 +0300

    Implement attempts counter in guessing game
    Added an attempts counter to track the number of guesses made by the user.

--- Assignment week four while loop task two.c
+++ Assignment week four while loop task two.c
@@ -5,11 +5,12 @@
 int main() {
 
     int secretNumber, guess;
+    int attempts = 0;   // Counter for number of guesses
 
     // Seed random number generator
     srand(time(0));
 
-    // Generate number between 1 and 20
+    // Generate random number between 1 and 20
     secretNumber = rand() % 20 + 1;
 
     printf("Guess a number between 1 and 20:\n");
@@ -17,6 +18,8 @@ int main() {
     while(1) {
         printf("Enter your guess: ");
         scanf("%d", &guess);
+
+        attempts++;   // Increase attempt count
 
         if(guess > secretNumber) {
             printf("Too high!\n");
@@ -26,9 +29,12 @@ int main() {
         }
         else {
             printf("Congratulations!\n");
+            printf("You guessed the number in %d attempts.\n", attempts);
             break;   // Exit loop when correct
         }
     }
 
     return 0;
 }
+
+
